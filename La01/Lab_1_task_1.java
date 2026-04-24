1b2628187abb183dcb86daa17d8d0fc4d9d4b991
dovbishmiroslav <dovbish.miroslav@gmail.com>
2026-04-24 08:14:22 +0300

    Delete La01 directory

--- La01/Lab_1_task_1.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Lab_1_task_1{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Name: "+name);
-    }
-}
